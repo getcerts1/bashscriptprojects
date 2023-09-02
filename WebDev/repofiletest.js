eadce0166045748f06ddb0e9c298c44ce5b2f106
Zakaria <barrowtech1999@gmail.com>
2023-09-02 15:58:17 +0300

    testing out commit on terminal

--- WebDev/repofiletest.js
+++ WebDev/repofiletest.js
@@ -5,3 +5,24 @@ for (let i=0; i < 20; i++) {
 }
 
 console.log(GenerateRandomNum)
+
+//this is a message
+
+array1 = [
+  {
+    name: 'product1',
+    rating: {
+      stars: 4.5,
+      count: 45
+    },
+    priceCents: 1099
+  },
+  {
+    name: 'product2',
+    rating: {
+      stars: 4.5,
+      count: 21
+    },
+    priceCents: 2045
+  }
+]
