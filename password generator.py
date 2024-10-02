e5b297c53482f3245663331325e719a5920a86e0
getcerts1 <135704716+getcerts1@users.noreply.github.com>
2024-10-02 03:11:27 +0300

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -1,10 +1,28 @@
 import random
+import string
 
-num_of_digits = int(input('select a number of digits for your password: > '))
-pin = ""
-for i in range(num_of_digits):
-    password = str(random.randint(1,9))
-    pin+=password
-    
-print(pin)
+name = input('what is your name? ')
+letters = int(input('how many letters would you like? '))
+symbols = int(input('how many symbols would you like? '))
+numbers = int(input('how many numbers would you like? '))
+total = letters + symbols + numbers
+
+
+random_char = []
+
+for i in range(1, letters + 1):
+  random_char.append(random.choice(string.ascii_lowercase))
+
+for i in range(1, symbols + 1):
+  random_char.append(random.choice(string.punctuation))
+
+for i in range(1, numbers + 1):
+  random_char.append(random.choice(string.digits))
+
+random.shuffle(random_char)
+password = ''.join(random_char)
+
+print(f'your password is -- {password} -- don\'t forget to change it after three months {name}')
+
+
 
